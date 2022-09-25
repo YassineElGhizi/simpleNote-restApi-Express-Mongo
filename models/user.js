bbf031919dafdf0b0087df1a28319d94d0a1a714
Yassineelghizi <yassine.ghizi2@gmail.com>
2022-09-25 18:22:59 +0100

    ready for search

--- models/user.js
+++ models/user.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String, required: true, unique: true
     }, password: {
         type: String, required: true
-    }, token: {type: String, default: ''}, // notes: [{
-    //     type: Schema.Types.ObjectId, ref: "Note"
-    // }]
-    notes: []
+    }, token: {type: String, default: ''}, notes: []
 })
 
+
+mongoose.set('toJSON', {
+    virtuals: true, transform: (doc, converted) => {
+        delete converted._id;
+    }
+})
 module.exports = mongoose.model("User", userSchema)
--- routes/notes.js
+++ routes/notes.js
@@ -12,33 +12,39 @@ router.get('/', function (req, res, next) {
 router.post('/', body('id').isString(), body('body').isString(), function (req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
-
     let note = {'id': mongoose.Types.ObjectId().toString(), 'body': req.body.body, 'created_at': Date.now()}
-    userModel.findByIdAndUpdate(req.body.id, {$push: {notes: note}}, {new: true}, (err, result) => {
+    userModel.findByIdAndUpdate(req.body.id, {$push: {notes: note}}, {
+        fields: {
+            'id': 1, 'token': 1, 'notes': 1, 'name': 1
+        }, new: true
+    }, (err, result) => {
         if (err) res.send(err)
-
         res.send(result)
     })
 })
 
-router.put('/:note_id', body('note_id').isString(), body('body').isString(), async (req, res, next) => {
+router.put('/:note_id', body('body').isString(), async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
 
-
-    userModel.find({"notes.id": "632f3bb574c71734cb7284c6"}, 'notes', (err, note) => {
+    userModel.findOne({"notes.id": req.params.note_id}, 'notes', async (err, user) => {
         if (err) return res.status(400).json({'status': 'No Record Matches'})
+        if (user === null) return res.status(400).json({'status': 'No Record Matches'})
 
-        console.log('😁', note)
-        res.send(note)
+        const newNotes = user.notes.map(note => {
+            if (note.id === req.params.note_id) return {...note, body: req.body.body}
+            return note;
+        })
+
+        userModel.findOneAndUpdate({"notes.id": req.params.note_id}, {$set: {notes: newNotes}}, {
+            fields: {
+                'id': 1, 'token': 1, 'notes': 1, 'name': 1
+            }, new: true
+        }, (err, result) => {
+            if (err) res.send(err)
+            res.send(result)
+        })
     })
-
-// userModel.findById(req.params.user_id, 'notes', (err, user) => {
-//     if (err || user === null) return res.status(400).json({'status': 'No Record Matches'})
-//
-//     res.send(user)
-// })
-
 })
 
 
