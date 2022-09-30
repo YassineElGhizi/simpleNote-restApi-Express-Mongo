32f29db2e5f7a0c571e51dcd5dae374a7960cd5c
Yassineelghizi <yassine.ghizi2@gmail.com>
2022-09-30 19:24:29 +0100

    full text search completed

--- routes/notes.js
+++ routes/notes.js
@@ -14,7 +14,7 @@ router.post('/', body('id').isString(), body('body').isString(), function (req, 
     if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
     let note = {'id': mongoose.Types.ObjectId().toString(), 'body': req.body.body, 'created_at': Date.now()}
     userModel.findByIdAndUpdate(req.body.id, {$push: {notes: note}}, {
-        fields: {
+        fieebodylbodyds: {
             'id': 1, 'token': 1, 'notes': 1, 'name': 1
         }, new: true
     }, (err, result) => {
@@ -44,6 +44,42 @@ router.put('/:note_id', body('body').isString(), async (req, res, next) => {
             if (err) res.send(err)
             res.send(result)
         })
+    })
+})
+
+router.delete('/:note_id', async (req, res, next) => {
+    userModel.findOne({"notes.id": req.params.note_id}, 'notes', async (err, user) => {
+        if (err) return res.status(400).json({'status': 'No Record Matches'})
+        if (user === null) return res.status(400).json({'status': 'No Record Matches'})
+
+        const newNotes = user.notes.filter(function (obj) {
+            return obj.id !== req.params.note_id;
+        });
+
+        userModel.findOneAndUpdate({"notes.id": req.params.note_id}, {$set: {notes: newNotes}}, {
+            fields: {
+                'id': 1, 'token': 1, 'notes': 1, 'name': 1
+            }, new: true
+        }, (err, result) => {
+            if (err) res.send(err)
+            res.send(result)
+        })
+    })
+})
+
+router.post('/search', body('keyword').isString(), body("user_id").isString(), async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+
+    userModel.findOne({"id": req.body.user_id}, 'notes', async (err, user) => {
+        if (err) return res.status(400).json({'status': 'No Record Matches'})
+        if (user === null) return res.status(400).json({'status': 'No Record Matches'})
+
+        const newNotes = user.notes.filter(function (obj) {
+            if (obj.body != null) return obj.body.includes(req.body.keyword);
+        });
+
+        res.send(newNotes)
     })
 })
 
